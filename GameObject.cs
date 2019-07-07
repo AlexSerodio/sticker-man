51cfaefaab89728f7e6720924da105ff9242a08e
AlexSerodio <alequis01@gmail.com>
2019-07-07 01:37:53 -0300

    add 3d camera projection

--- GameObject.cs
+++ GameObject.cs
@@ -90,7 +90,7 @@ namespace stick_man
             GL.LineWidth(3);
             GL.Begin(primitive);
                 foreach(Ponto4D vertex in this.vertices)
-                    GL.Vertex2(vertex.X, vertex.Y);
+                    GL.Vertex3(vertex.X, vertex.Y, vertex.Z);
             GL.End();
 
             GL.PopMatrix();
--- Render.cs
+++ Render.cs
@@ -13,15 +13,40 @@ namespace stick_man
     private World world;
     private Stickman player;
 
-    public Render(int width, int height) : base(width, height) { 
+    // Eye: (87, 246, 257)
+    // Target: (78, 255, -74)
+    Vector3 eye = new Vector3(0, 0, 500);
+    Vector3 target = Vector3.Zero;
+    Vector3 up = Vector3.UnitY;
+
+    public Render(int width, int height) : base(width, height)
+    { 
 
       world = new World(width, height);
-      player = new Stickman(new Ponto4D(200, 1200), 0.2, world);
+      // player = new Stickman(new Ponto4D(200, 1200), 0.2, world);
+      player = new Stickman(new Ponto4D(0, 0), 0.2, world);
 
     }
 
-    protected override void OnLoad(EventArgs e) { 
+    protected override void OnLoad(EventArgs e)
+    { 
       base.OnLoad(e); 
+
+      GL.ClearColor(Color.Gray);
+      GL.Enable(EnableCap.DepthTest);
+      GL.Enable(EnableCap.CullFace);
+
+    }
+
+    protected override void OnResize(EventArgs e) 
+    {
+      base.OnResize(e);
+
+      GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
+
+      Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width / (float)Height, 1.0f, 600.0f);
+      GL.MatrixMode(MatrixMode.Projection);
+      GL.LoadMatrix(ref projection);
     }
 
     protected override void OnUpdateFrame(FrameEventArgs e)
@@ -31,16 +56,17 @@ namespace stick_man
       if(!world.IsCreationModeOn())
         HandlePlayerMovement();
 
-      world.UpdateCamera();
+      // world.UpdateCamera();
     }
 
     protected override void OnRenderFrame(FrameEventArgs e)
     {
       base.OnRenderFrame(e);
 
-      GL.Clear(ClearBufferMask.ColorBufferBit);
-      GL.ClearColor(Color.Gray);
+      GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+      Matrix4 modelview = Matrix4.LookAt(eye, target, up);
       GL.MatrixMode(MatrixMode.Modelview);
+      GL.LoadMatrix(ref modelview);
 
       world.DrawObjects();
       player.Draw();
@@ -117,6 +143,52 @@ namespace stick_man
           world.GetLastObject().SetPrimitive(PrimitiveType.LineLoop);
           world.SetCreatingObject(false);
           break;
+        case Key.Right:
+          eye.X += 1;
+          break;
+        case Key.Left:
+          eye.X -= 1;
+          break;
+        case Key.Up:
+          eye.Y += 1;
+          break;
+        case Key.Down:
+          eye.Y -= 1;
+          break;
+        case Key.Home:
+          eye.Z += 1;
+          break;
+        case Key.End:
+          eye.Z -= 1;
+          break;
+        
+        case Key.L:
+          target.X += 1;
+          break;
+        case Key.J:
+          target.X -= 1;
+          break;
+        case Key.I:
+          target.Y += 1;
+          break;
+        case Key.K:
+          target.Y -= 1;
+          break;
+        case Key.N:
+          target.Z += 1;
+          break;
+        case Key.M:
+          target.Z -= 1;
+          break;        
+
+        case Key.Escape:
+          this.Exit();
+          break;
+
+        case Key.P:
+          Console.WriteLine("Eye: ({0}, {1}, {2})", eye.X, eye.Y, eye.Z);
+          Console.WriteLine("Target: ({0}, {1}, {2})", target.X, target.Y, target.Z);
+        break;
       }
     }
   }
